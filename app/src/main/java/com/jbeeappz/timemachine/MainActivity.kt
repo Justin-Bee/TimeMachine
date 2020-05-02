41847436890e4b8720310f85682cc3e88348b2a6
Justin <jbee.appz@gmail.com>
2020-05-02 13:28:58 -0400

    beta version

--- app/src/main/java/com/jbeeappz/timemachine/MainActivity.kt
+++ app/src/main/java/com/jbeeappz/timemachine/MainActivity.kt
@@ -1,12 +1,48 @@
 package com.jbeeappz.timemachine
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ProgressBar
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Handler
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
 
+    private var progressBar: ProgressBar? = null
+    private var i = 0
+    private val handler = Handler()
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        progressBar = findViewById<ProgressBar>(R.id.progressBar) as ProgressBar
+        val button: Button = findViewById<Button>(R.id.button)
+        button?.setOnClickListener(){
+            i = progressBar!!.progress
+            Thread(Runnable {
+                while (i < 100) {
+                    i += 5
+                    // Update the progress bar and display the current value
+                    handler.post(Runnable {
+                        progressBar!!.progress = i
+
+                    })
+                    try {
+                        Thread.sleep(100)
+                    } catch (e: InterruptedException) {
+                        e.printStackTrace()
+                    }
+
+                }
+            }).start()
+            var str = "Welcome to your destination"
+            Toast.makeText(this@MainActivity, str, Toast.LENGTH_LONG).show()
+        }
     }
+
+
 }
